1a92157421e563f9c145cc6901871dad3cdb498c
Irakli <47250537+Kr1ner@users.noreply.github.com>
2023-07-28 20:41:43 +0400

    update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -12,15 +12,15 @@ function App() {
             <h1 className='font-main text-[#8F60A9] text-[3.1vw]'>Read, review and track <br/>your book reading <br/> progress.</h1>
             <h5 className='font-secondary text-right text-[#D196FF] font-semibold'>First online website for making your book tracking experience better and many more features combined in one website.</h5>
             <section className='w-full flex flex-row justify-between'>
-              <Link style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center bg-[#8F60A9] px-5 py-2 rounded-full text-[#E5E5E5] font-secondary text-[1vw] font-semibold' to={'/register'}>START YOUR JOURNEY TODAY</Link>
-              <Link style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center  px-5 py-2 rounded-full text-[#8F60A9] font-secondary text-[1vw] border-2 border-[#8F60A9] font-semibold' to={'/register'}>START YOUR JOURNEY TODAY</Link>
+              <Link style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center bg-[#8F60A9] px-5 py-2 rounded-full text-[#E5E5E5] font-secondary text-[1vw] font-semibold hover:bg-[#D196FF]' to={'/register'}>START YOUR JOURNEY TODAY</Link>
+              <Link style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center  px-5 py-2 rounded-full text-[#8F60A9] font-secondary text-[1vw] border-2 border-[#8F60A9] font-semibold hover:border-[#D196FF] hover:text-[#D196FF]' to={'/register'}>START YOUR JOURNEY TODAY</Link>
             </section>
           </div>
           <div id='homeheaddimg' className='w-[29vw] h-fit'>
             <img src={FloatingBook} alt="logo" />
           </div>
         </header>
-        <img onClick={()=>{let x=window.innerHeight; window.scrollBy(0,x)}} className='w-5 hover:cursor-pointer' src={msdwn} alt='mousedownicon'></img>
+        <img onClick={()=>{let x=window.innerHeight; window.scrollTo(0,x)}} className='w-5 hover:cursor-pointer' src={msdwn} alt='mousedownicon'></img>
       </div>
       <section className='w-full h-[50vh] mt-[5vh] flex flex-row items-center justify-center' id='homecards'>
         <ul className='w-2/3 h-3/4 flex flex-row justify-around' id='cardsholder'>
@@ -29,11 +29,11 @@ function App() {
             <h3>What is Mark.It</h3>
             <p></p>
           </li>
-          <li id='card2' className='w-1/4 h-full bg-[#D196FF]/[.4] rounded-[15%]'>
+          <li style={{"filter":"drop-shadow(10px 18px 5px rgb(239 190 222))"}} id='card2' className='w-1/4 h-full bg-[#D196FF]/[.4] rounded-[15%]'>
           <div id='imgholder2'></div>
 
           </li>
-          <li id='card3' className='w-1/4 h-full bg-[#D196FF]/[.4] rounded-[15%]'>
+          <li style={{"filter":"drop-shadow(10px 18px 5px rgb(239 190 222))"}} id='card3' className='w-1/4 h-full bg-[#D196FF]/[.4] rounded-[15%]'>
           <div id='imgholder3'></div>
           </li>
         </ul>
--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -6,10 +6,10 @@ export default function NavBar(){
         <nav className='flex h-fit w-3/5 flex-row justify-between ' id="navbar">
             <NavLink to={'/'}><img style={{"filter":"drop-shadow(2px 3px 2px rgb(143 96 169 / 0.7))"}} className='h-[7vh]' src={logo} alt="Mark.It Logo" /></NavLink>
             <div className='w-1/2 flex flex-row justify-between place-items-center font-main'>
-                <NavLink className='text-[#D196FF] text-[0.9vw]' to={'/'}>HOME</NavLink>
-                <NavLink className='text-[#D196FF] text-[0.9vw]' to={'/personal'}>PERSONAL</NavLink>
-                <NavLink className='text-[#D196FF] text-[0.9vw]' to={'/signin'}>SIGN IN</NavLink>
-                <NavLink style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center bg-[#8F60A9] px-5 py-2 rounded-md text-[#E5E5E5] text-[0.9vw]' to={'/register'}>JOIN NOW</NavLink>
+                <NavLink className='text-[#D196FF] text-[0.9vw] duration-100 hover:text-[#8F60A9] duration-100' to={'/'}>HOME</NavLink>
+                <NavLink className='text-[#D196FF] text-[0.9vw] hover:text-[#8F60A9] duration-100' to={'/personal'}>PERSONAL</NavLink>
+                <NavLink className='text-[#D196FF] text-[0.9vw] hover:text-[#8F60A9] duration-100' to={'/signin'}>SIGN IN</NavLink>
+                <NavLink style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center bg-[#8F60A9] px-5 py-2 rounded-md text-[#E5E5E5] text-[0.9vw] duration-100 hover:bg-[#D196FF] duration-100' to={'/register'}>JOIN NOW</NavLink>
             </div>
         </nav>
     )

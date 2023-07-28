12e051a585841aec0a83b7baa21b2d544241acad
Irakli <47250537+Kr1ner@users.noreply.github.com>
2023-07-28 23:06:37 +0400

    update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -12,8 +12,8 @@ function App() {
             <h1 className='font-main text-[#8F60A9] text-[3.1vw]'>Read, review and track <br/>your book reading <br/> progress.</h1>
             <h5 className='font-secondary text-right text-[#D196FF] font-semibold'>First online website for making your book tracking experience better and many more features combined in one website.</h5>
             <section className='w-full flex flex-row justify-between'>
-              <Link style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center bg-[#8F60A9] px-5 py-2 rounded-full text-[#E5E5E5] font-secondary text-[1vw] font-semibold hover:bg-[#D196FF]' to={'/register'}>START YOUR JOURNEY TODAY</Link>
-              <Link style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center  px-5 py-2 rounded-full text-[#8F60A9] font-secondary text-[1vw] border-2 border-[#8F60A9] font-semibold hover:border-[#D196FF] hover:text-[#D196FF]' to={'/register'}>START YOUR JOURNEY TODAY</Link>
+              <Link style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center bg-[#8F60A9] px-5 py-2 rounded-full text-[#E5E5E5] font-secondary text-[1vw] font-semibold hover:bg-[#D196FF] duration-100' to={'/register'}>START YOUR JOURNEY TODAY</Link>
+              <Link style={{"filter":"drop-shadow(2px 3px 4px rgb(143 96 169 / 0.4))"}} className='inline-block text-center  px-5 py-2 rounded-full text-[#8F60A9] font-secondary text-[1vw] border-2 border-[#8F60A9] font-semibold hover:border-[#D196FF] hover:text-[#D196FF] duration-100' to={'/register'}>START YOUR JOURNEY TODAY</Link>
             </section>
           </div>
           <div id='homeheaddimg' className='w-[29vw] h-fit'>
